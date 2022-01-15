c50f309bc9af6474aac7a3c35de0443aa1b8eedd
Shyunju <hyunju9908@naver.com>
2022-01-15 20:36:58 +0900

    hyunju reset test

--- Assets/Scenes/change.cs
+++ Assets/Scenes/change.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-//tr
+
 public class change : MonoBehaviour
 {
     public void SceneChange()
